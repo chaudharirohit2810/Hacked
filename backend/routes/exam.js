d7afb89ea85ed06607078d25bfda7e8618e2687e
Rohit Chaudhari <chaudharirohit2810@gmail.com>
2021-02-03 10:16:36 +0530

    chore: added download button, route in backed for download

--- backend/routes/exam.js
+++ backend/routes/exam.js
@@ -4,9 +4,36 @@ const Exam = require("../models/exam");
 router.route("/").get(async (req, res) => {
     try {
         var exams = await Exam.find();
-        res.status(200).send(exams);
+        exams = JSON.stringify(exams);
+        exams = JSON.parse(exams);
+
+        var data = exams.map((exam) => {
+            const { Questions, ...temp } = exam;
+            var totalMarks = 0;
+            for (var index in Questions) {
+                totalMarks += parseInt(Questions[index]["marks"]);
+            }
+            return {
+                totalQuestions: Questions.length,
+                totalMarks,
+                ...temp,
+            };
+        });
+
+        res.status(200).send(data);
     } catch (err) {
+        console.log(err.message);
         res.status(400).send(err.message);
+    }
+});
+
+router.route("/download").get(async (req, res) => {
+    try {
+        const id = req.header("id");
+        const exam = await Exam.findById(id);
+        res.status(200).send(exam);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 });
 
--- src/pages/user/home/examCard.jsx
+++ src/pages/user/home/examCard.jsx
@@ -1,8 +1,17 @@
-import { Card, CardContent, Typography } from "@material-ui/core";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Typography,
+} from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
-import { secureStorage } from "../../../config";
+import { backendURL, secureStorage } from "../../../config";
 import Style from "./home.module.scss";
+import DownloadIcon from "@material-ui/icons/CloudDownloadOutlined";
+import ExamIcon from "@material-ui/icons/DoneOutlineRounded";
+import axios from "axios";
 
 const ExamCard = ({ item, index }) => {
     const formatDate = (date) => {
@@ -17,48 +26,61 @@ const ExamCard = ({ item, index }) => {
         return strTime;
     };
 
-    const calculateMarks = () => {
-        let totalMarks = 0;
-        for (var it in item.Questions) {
-            totalMarks += parseInt(item.Questions[it]["marks"]);
-        }
-        return totalMarks;
+    const downloadExam = () => {
+        axios
+            .get(`${backendURL}/exam/download`, { headers: { id: item._id } })
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
 
     return (
-        <Link
-            to="/exam"
-            className={Style.card_link}
-            onClick={() => secureStorage.setItem("selectedQuiz", index)}
-        >
-            <Card
-                variant="outlined"
-                elevation={4}
-                style={{ marginTop: "2rem" }}
-            >
-                <CardContent>
-                    <Typography color="textPrimary" variant="h6">
-                        {item.title || "N/A"}
-                    </Typography>
-                    <Typography variant="subtitle1" gutterBottom>
-                        {item.subTitle || "N/A"}
-                    </Typography>
-                    <Typography color="textSecondary" variant="body2">
-                        <b>Start Time: </b> {formatDate(item.startTime)}
-                    </Typography>
-                    <Typography color="textSecondary" variant="body2">
-                        <b>End Time: </b> {formatDate(item.endTime)}
-                    </Typography>
-                    <Typography color="textSecondary" variant="body2">
-                        <b>Number of Questions: </b> {item.Questions.length}
-                    </Typography>
+        <Card variant="outlined" elevation={4} style={{ marginTop: "2rem" }}>
+            <CardContent>
+                <Typography color="textPrimary" variant="h6">
+                    {item.title || "N/A"}
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                    {item.subTitle || "N/A"}
+                </Typography>
+                <Typography color="textSecondary" variant="body2">
+                    <b>Start Time: </b> {formatDate(item.startTime)}
+                </Typography>
+                <Typography color="textSecondary" variant="body2">
+                    <b>End Time: </b> {formatDate(item.endTime)}
+                </Typography>
+                <Typography color="textSecondary" variant="body2">
+                    <b>Number of Questions: </b> {item.totalQuestions}
+                </Typography>
 
-                    <Typography color="textSecondary" variant="body2">
-                        <b>Total Marks: </b> {calculateMarks()}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Link>
+                <Typography color="textSecondary" variant="body2">
+                    <b>Total Marks: </b> {item.totalMarks}
+                </Typography>
+            </CardContent>
+            <CardActions style={{ paddingBottom: "1rem", paddingLeft: "1rem" }}>
+                <Button
+                    color="primary"
+                    variant="contained"
+                    size="small"
+                    startIcon={<DownloadIcon />}
+                    onClick={downloadExam}
+                >
+                    Download
+                </Button>
+                <Button
+                    color="secondary"
+                    variant="contained"
+                    size="small"
+                    disabled={!secureStorage.getItem(item._id)}
+                    startIcon={<ExamIcon />}
+                >
+                    Start exam
+                </Button>
+            </CardActions>
+        </Card>
     );
 };
 
